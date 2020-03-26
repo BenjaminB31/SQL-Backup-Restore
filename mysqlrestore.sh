b4ee61cd8d23de957316c39e9f0002774f11a709
Benjamin Blanchard <56205597+BenjaminB31@users.noreply.github.com>
2020-03-26 12:32:41 +0100

    Add files via upload

--- mysqlrestore.sh
+++ mysqlrestore.sh
@@ -13,7 +13,7 @@ echo "$(ls $BACKUP_DIR)"
 read -p "Date de la backup: " date
 
 if [ -d "$BACKUP_DIR/$date" ];then
- echo "Le dossier $date existe";
+ echo -e "Liste des bases disponnible :";
 else
 echo "Le dossier n'existe pas !";
 exit;
@@ -23,7 +23,7 @@ echo "$(cd $BACKUP_DIR/$date; for f in *.sql.gz; do printf "%s\n" "${f%.sql.gz}"
 read -p "Nom de la base à restaurer: " db
 
 if [ -f "$BACKUP_DIR/$date/$db.sql.gz" ];then
- echo "La base $db existe";
+ echo -e "\n";
 else
 echo "La base n'existe pas !";
 exit;
@@ -31,6 +31,28 @@ fi
 
 MYSQL=/usr/bin/mysql
 
-gunzip $BACKUP_DIR/$date/$db.sql.gz
-$MYSQL --force --user=$MYSQL_USER --password=$MYSQL_PASSWORD < "$BACKUP_DIR/$date/$db.sql"
-gzip $BACKUP_DIR/$date/$db.sql
+if [[ `$MYSQL --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e "USE ${db};" 2> /tmp/error.logextract ; cat /tmp/error.logextract` = "ERROR 1049 (42000) at line 1: Unknown database '${db}'" ]];then
+	rm /tmp/error.logextract;
+    echo "La base n'existe pas et va être crée";
+    gunzip $BACKUP_DIR/$date/$db.sql.gz;
+    $MYSQL --force --user=$MYSQL_USER --password=$MYSQL_PASSWORD < "$BACKUP_DIR/$date/$db.sql";
+    gzip $BACKUP_DIR/$date/$db.sql;
+    echo "Import effectuer";
+    exit;
+else
+    rm /tmp/error.logextract;
+    echo "La base existe déja";
+    read -p "Voulez vous supprimer les données actuelles pour réinsérer la backup (y/n): " action
+    
+    if [ $action == "y" ]
+    then
+        $MYSQL --user=$MYSQL_USER --password=$MYSQL_PASSWORD -e "DROP DATABASE ${db};"
+        gunzip $BACKUP_DIR/$date/$db.sql.gz;
+        $MYSQL --force --user=$MYSQL_USER --password=$MYSQL_PASSWORD < "$BACKUP_DIR/$date/$db.sql";
+        gzip $BACKUP_DIR/$date/$db.sql;
+        echo "Import effectuer";
+        exit;
+    else
+        exit;
+    fi
+fi
